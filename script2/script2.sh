9c08c912b733e112d2fbc4cbc4d56424d1410bb3
Fantomas4 <Fantomas4@users.noreply.github.com>
2018-11-15 13:50:52 +0200

    Fixed a bug that caused the script to read .txt files inside the downloaded repositories folders. In addition, the Cloning FAILED message is now printed in the stderr.

--- script2/script2.sh
+++ script2/script2.sh
@@ -8,38 +8,17 @@ txt_files_dir_array=()
 # Unzip the given .tar.gz file
 unzip $input_zip_file_dir -d $unzipped_files_dir > /dev/null 2>&1
 
-# Find all .txt files and store them into an array
-mapfile -t txt_files_dir_array < <( find . -type f -name "*.txt" )
-
-#echo "----------------------- DIAG START -----------------------"
-#
-#for txt_dir in "${txt_files_dir_array[@]}"
-#do
-#	#echo "mpika3-------------------"
-#	#echo $queue_entry
-#	#echo "-------------------------"
-#	echo $txt_dir
-#	#echo "" >> $webpages_queue_dir
-#
-#done
-#
-#echo "----------------------- DIAG END -----------------------"
+# Find all .txt files in the unzipped_files folder and store them into an array
+mapfile -t txt_files_dir_array < <( find ./unzipped_files -type f -name "*.txt" )
 
 # For every .txt file directory stored in txt_files_dir_array, read the git repo url the given
 # .txt file contains
-
 for txt_dir in "${txt_files_dir_array[@]}"
 do
 	#echo $txt_dir
-	
-	#read -r repo_url < "$txt_dir"
-	
-	#echo $repo_url
-	
-	
+
 	# Find the first line in the .txt file that is NOT a comment
 	# and contains a git repo url
-	
 	while IFS= read -r txt_line 
 	do	
 		# ${input_url:0:1} expands to the substring starting at position 
@@ -52,31 +31,28 @@ do
 			break
 		fi
 	done < "$txt_dir"
-	
-	echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
-	echo $repo_url
-	echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
-	
-	
-	
+
 	# Extract the repository name from the repository url
 	repo_name="$(basename $repo_url)"
 	
 	# Attempt to clone the given repo_url from github
 	git clone $repo_url "$repos_clone_dir/$repo_name" > /dev/null 2>&1
 
-	clone_status=$?
-	echo $clone_status
+	clone_status=$? 
+	#echo $clone_status
 
 	if [ $clone_status == 0 ]; then
-
+		
+		# Print message to stdout
 		echo "$repo_url: Cloning OK"
 
 	else
-
-		echo "$repo_url: Cloning FAILED"
+		# Print message to stderr
+		echo "$repo_url: Cloning FAILED" >&2
 
 	fi
+	
+	#echo -e "\n"
 
 done
 
